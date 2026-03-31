7352b64c78e930ed9677860fd4f14e66fd2e8ea9
Kamalesh Paramadayalan <kamaleshdaya2006@gmail.com>
2026-03-31 21:00:06 +0530

    Update hiddengems.py

--- Backend/routes/hiddengems.py
+++ Backend/routes/hiddengems.py
@@ -3,7 +3,7 @@ from db import get_connection
 
 hidden_gems_bp = Blueprint("hidden_gems", __name__)
 
-@hidden_gems_bp.route("/hidden-gems", methods=["GET"])
+@hidden_gems_bp.route("/", methods=["GET"])
 def get_hidden_gems():
     conn = get_connection()
     cursor = conn.cursor()
