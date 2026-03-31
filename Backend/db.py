023a018acba054547313871ab3c257c7eba9ffef
Kamalesh Paramadayalan <kamaleshdaya2006@gmail.com>
2026-03-31 19:59:25 +0530

    Update db.py

--- Backend/db.py
+++ Backend/db.py
@@ -2,7 +2,10 @@ import os
 import psycopg2
 
 def get_connection():
+    db_url = os.getenv("DATABASE_URL")
+    print("DB URL:", db_url)  # debug
+
     return psycopg2.connect(
-        os.getenv("DATABASE_URL"),
+        db_url,
         sslmode="require"
     )
